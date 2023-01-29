b44d4282d14ed4cfec3ae12b413abf11acb1093e
Matias Paavilainen <matias.paavilainen@gmail.com>
2023-01-29 17:21:41 +0200

    2c

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,24 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/filter'
 import Form from './components/contactForm'
 import Numbers from './components/contacts'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter persons={persons} setPersons={setPersons}/>
+      <Filter persons={persons} setPersons={setPersons} />
       <h2>Add contact</h2>
       <Form persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-      <Numbers persons={persons}/>
+      <Numbers persons={persons} />
     </div>
   )
 }
