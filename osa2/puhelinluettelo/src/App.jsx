5a0bdb594fe3fb1e0ade574fd82dc24a0c194214
Matias Paavilainen <101323404+matiaspaavilainen@users.noreply.github.com>
2024-05-06 10:57:54 +0300

    linter change

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -8,36 +8,36 @@ import numberService from './services/numberService'
 import Notification from './components/notification'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [type, setType] = useState(null)
-  
-  useEffect(() => {
-    numberService.getAll()
-      .then(initialPersons => {
-        setPersons(initialPersons)
-      })
-  }, [])
-  
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <Notification message={errorMessage} type={type} />
-      <Filter persons={persons} setPersons={setPersons} />
-      <Form 
-        persons={persons} 
-        setPersons={setPersons} 
-        setErrorMessage={setErrorMessage} 
-        setType={setType} 
-        />
-      <AllPersons 
-        persons={persons} 
-        setPersons={setPersons} 
-        setErrorMessage={setErrorMessage}
-        setType={setType} 
-        />
-    </div>
-  )
+	const [persons, setPersons] = useState([])
+	const [errorMessage, setErrorMessage] = useState(null)
+	const [type, setType] = useState(null)
+
+	useEffect(() => {
+		numberService.getAll()
+			.then(initialPersons => {
+				setPersons(initialPersons)
+			})
+	}, [])
+
+	return (
+		<div>
+			<h1>Phonebook</h1>
+			<Notification message={errorMessage} type={type} />
+			<Filter persons={persons} setPersons={setPersons} />
+			<Form
+				persons={persons}
+				setPersons={setPersons}
+				setErrorMessage={setErrorMessage}
+				setType={setType}
+			/>
+			<AllPersons
+				persons={persons}
+				setPersons={setPersons}
+				setErrorMessage={setErrorMessage}
+				setType={setType}
+			/>
+		</div>
+	)
 }
 
 export default App
